30c5af2709acd12943ffecc29d5499e38bb58ca3
Sara Eide-Altman <SaraRose686@gmail.com>
2019-06-23 17:09:17 -0700

    Connected to APIs

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 "use strict";
 
-// put your own value below!
-const apiKey = "0SOJv0wMquzXnxgWYVTW18Sq9WZPNmiCaWbD7ZjZ";
-const searchURL = "https://developer.nps.gov/api/v1/parks";
+const api = {
+    petfinder: {
+        client: new petfinder.Client( { // Petfinder JS SDK client to perform API calls
+                apiKey: "JI9fv1y8atpa4x8Rw3MsC1oOXADcFk13dptH2digjtnRbTgWp2", 
+                secret: "N9mNCi9sVSp6EOKfvygtvWdTdYjaEv62TUm8Jbyn"}),
+        searchParams: {
+            type: "Dog",
+            status: "adoptable",
+            location: "95814",
+            distance: 25,
+            sort: "distance",
+            page: 1,
+            limit: 26,
+        }
+    },
+    getYourPet: {
+        search: {
+            url: "https://getyourpet.com/api/partnerpetsearch",
+            data: {
+                method: "POST",
+                body: null,
+                headers: { 
+                    "Content-Type": "application/json",
+                    //"Access-Control-Allow-Origin:": "http://localhost:5500/"
+                },
+            },
+            params: {
+                PetType: "dog",
+                ZipCode: "95814",
+                SearchRadiusInMiles: 25,
+                PageNumber: 1,
+                OrderBy: "distance",
+            }
+        }
+    }
+};
 
 function formatQueryParams(params) {
     const queryItems = Object.keys(params).map(
@@ -15,35 +48,38 @@ function displayResults(responseJson) {
     // if there are previous results, remove them
     console.log(responseJson);
     $("#results-list").empty();
+
     // iterate through the items array
-    for (let i = 0; i < responseJson.data.length; i++) {
-        // for each park in the data
-        //array, add a list item to the results
-        //list with the park name, description,
-        //and url
-        $("#results-list").append(
-            `<li><h3>${responseJson.data[i].fullName}</h3>
-      <p>${responseJson.data[i].description}</p>
-      <a href='${responseJson.data[i].url}' target="_blank">${responseJson.data[i].url}</a>
-      </li>`
-        );
-    }
+    // for (let i = 0; i < responseJson.data.length; i++) {
+    //     // for each park in the data
+    //     //array, add a list item to the results
+    //     //list with the park name, description,
+    //     //and url
+    //     $("#results-list").append(
+    //         `<li><h3>${responseJson.data[i].fullName}</h3>
+    //   <p>${responseJson.data[i].description}</p>
+    //   <a href='${responseJson.data[i].url}' target="_blank">${responseJson.data[i].url}</a>
+    //   </li>`
+    //     );
+    // }
+
     //display the results section
     $("#results").removeClass("hidden");
 }
 
-function getNatParks(state, limit = 10) {
-    const params = {
-        api_key: apiKey,
-        stateCode: state,
-        limit
-    };
-    const queryString = formatQueryParams(params);
-    const url = searchURL + "?" + queryString;
+function getPetList() {
+    
+    // Search Petfinder
+    api.petfinder.client.animal.search(api.petfinder.searchParams)
+        .then( response => displayResults(response))
+        .catch( error => {
+            // Handle the error
+            $("#js-error-message").text(`Something went wrong: ${error.message}`);
+        });
 
-    console.log(url);
-
-    fetch(url)
+    // Search Get Your Pet
+    api.getYourPet.search.data.body = JSON.stringify(api.getYourPet.search.params);
+    fetch(api.getYourPet.search.url, api.getYourPet.search.data)
         .then(response => {
             if (response.ok) {
                 return response.json();
@@ -56,21 +92,10 @@ function getNatParks(state, limit = 10) {
         });
 }
 
-function parseStateSelection() {
-    const stateList = [];
-    $("#js-search-state > option:selected").each( function() {
-        stateList.push( this.value );
-    })
-    return stateList.join(",");
-    
-}
-
 function watchForm() {
     $("form").submit(event => {
         event.preventDefault();
-        const searchState = parseStateSelection();
-        const maxResults = $("#js-max-results").val();
-        getNatParks(searchState, maxResults);
+        getPetList();
     });
 }
 
