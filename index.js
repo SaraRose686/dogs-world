bf3280783888a7b09d2c1e5c3aa7711fbe5972c4
Sara Eide-Altman <SaraRose686@gmail.com>
2019-06-25 23:00:47 -0700

    Completed search filters

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ const dogOptions = {
         small: "Small",
         medium: "Medium",
         large: "Large",
-        xLarge: "X-Large"
+        xlarge: "X-Large"
     },
     filter: {
-        petfinder: {
-            size: ["small", "medium", "large", "xlarge"]
-        },
         getYourPet : {
-            size: {
-                values: [ "extrasmall", "small", "medium", "large", "extralarge"],
-                params: [ "SizeMin", "SizeMax"]
-            }
+            age: {
+                baby: [0, 1],
+                young: [1, 2],
+                adult: [2, 7],
+                senior: [7, 20]
+            },
+            size: [ "extrasmall", "small", "medium", "large", "extralarge"],
         }
     }
 };
@@ -33,6 +33,7 @@ const dogOptions = {
 const api = {
     results: [],
     petfinder: {
+        displaySearch: true,
         client: new petfinder.Client( { // Petfinder JS SDK client to perform API calls
                 apiKey: "JI9fv1y8atpa4x8Rw3MsC1oOXADcFk13dptH2digjtnRbTgWp2", 
                 secret: "N9mNCi9sVSp6EOKfvygtvWdTdYjaEv62TUm8Jbyn"}),
@@ -47,6 +48,7 @@ const api = {
         }
     },
     getYourPet: {
+        displaySearch: true,
         search: {
             url: "https://cors-anywhere.herokuapp.com/https://getyourpet.com/api/partnerpetsearch",
             data: {
@@ -89,12 +91,14 @@ function displayResults() {
             `<li>
                 <h3>${api.results[i].name}</h3>
                 <img src='${api.results[i].photoURL}' alt='Picture of ${api.results[i].name}'>
+                <button>View Details</button>
             </li>`
         );
     }
 
     //display the results section
     $("#results").removeClass("hidden");
+    $(".additionalSearch").removeClass("hidden");
 }
 
 function processPetFinderResults( responseJson ) {
@@ -109,7 +113,7 @@ function processPetFinderResults( responseJson ) {
             ( pet.age === "Senior" ? dogOptions.age.senior : dogOptions.age.adult ) ),
         size: pet.size === "Small" ? dogOptions.size.small : 
             ( pet.size === "Medium" ? dogOptions.size.medium : 
-            ( pet.size === "Large" ? dogOptions.size.large : dogOptions.size.xLarge ) ),
+            ( pet.size === "Large" ? dogOptions.size.large : dogOptions.size.xlarge ) ),
         photoURL: pet.photos[0].medium
     }));
         
@@ -128,7 +132,7 @@ function processGetYourPetResults( responseJson ) {
             ( pet.AgeYears > 6 ? dogOptions.age.senior : dogOptions.age.adult ) ),
         size: pet.Size === "s (11lbs. to 20lbs.)" || pet.Size === "xs (10lbs. or less)" ? dogOptions.size.small :
             ( pet.Size === "m (21lbs. to 40lbs.)" ? dogOptions.size.medium : 
-            ( pet.Size === "xl (over 75lbs.)" ? dogOptions.size.xLarge : dogOptions.size.large ) ),
+            ( pet.Size === "xl (over 75lbs.)" ? dogOptions.size.xlarge : dogOptions.size.large ) ),
         photoURL: pet.PrimaryPhotoUrl
     }));
         
@@ -154,8 +158,12 @@ function getPetList() {
 
     Promise.all([ getYourPetRequest, petfinderRequest ])
         .then( responses => {
-            processGetYourPetResults(responses[0]);
-            processPetFinderResults(responses[1]);
+            if( api.getYourPet.displaySearch ) {
+                processGetYourPetResults(responses[0]);
+            }
+            if( api.petfinder.displaySearch ) {
+                processPetFinderResults(responses[1]);
+            }
         })
         .then( () => displayResults())
         .catch(err => {
@@ -163,17 +171,140 @@ function getPetList() {
         });
 }
 
-function updateZipCode() {
-    const zipInput = $("#js-zip-code").val();
+function cleanFilters() {
+    api.getYourPet.displaySearch = true;
+    api.petfinder.displaySearch = true;
+    delete api.petfinder.searchParams.age;
+    delete api.getYourPet.search.params.AgeYearsMin;
+    delete api.getYourPet.search.params.AgeYearsMax;
+    delete api.petfinder.searchParams.gender;
+    delete api.getYourPet.search.params.Gender;
+    delete api.petfinder.searchParams.size;
+    delete api.getYourPet.search.params.SizeMin;
+    delete api.getYourPet.search.params.SizeMax;
+}
+
+
+// Read user input on Age filter
+function readAge() {
+    const ageInput = [];
+    $(".js-age:checked").each( function() {
+        ageInput.push($(this).val());
+    } );
+
+    if( ageInput.length > 0 ) {
+        api.petfinder.searchParams.age = ageInput.join();
+        
+        // create age search parameter for GetYourPet API
+        for( let [ageKey, ageValue] of Object.entries(dogOptions.filter.getYourPet.age) ) {
+            if( ageInput.includes(ageKey) ) {
+                api.getYourPet.search.params.AgeYearsMin = 
+                    api.getYourPet.search.params.AgeYearsMin < ageValue[0] ?
+                    api.getYourPet.search.params.AgeYearsMin : ageValue[0];
+                api.getYourPet.search.params.AgeYearsMax = 
+                    api.getYourPet.search.params.AgeYearsMax > ageValue[1] ?
+                    api.getYourPet.search.params.AgeYearsMax : ageValue[1];
+            }
+        }
+    }
+}
+
+// Read user input on Caretaker filter
+function readCaretaker() {
+    const caretakerInput = [];
+    $(".js-caretaker:checked").each( function() {
+        caretakerInput.push($(this).val());
+    } );
+    
+    if( caretakerInput.length === 1 ) {
+        if(caretakerInput[0] === "getYourPet") {
+            api.petfinder.displaySearch = false;
+        }
+        else {
+            api.getYourPet.displaySearch = false;
+        }
+    }
+}
+
+// Read user input on Gender filter
+function readGender() {
+    const genderInput = [];
+    $(".js-gender:checked").each( function() {
+        genderInput.push($(this).val());
+    } );
+    if( genderInput.length > 0 ) {
+        api.petfinder.searchParams.gender = genderInput.join();
+        api.getYourPet.search.params.Gender = genderInput.join();
+    }
+    
+}
+
+// Read user input on Size filter
+function readSize() {
+    const sizeInput = [];
+    $(".js-size:checked").each( function() {
+        sizeInput.push($(this).val());
+    } );
+    if( sizeInput.length > 0 ) {
+        api.petfinder.searchParams.size = sizeInput.join();
+        
+        if( sizeInput.includes("small") ) {
+            api.getYourPet.search.params.SizeMin = 
+                dogOptions.filter.getYourPet.size[0];
+
+            api.getYourPet.search.params.SizeMax = 
+                dogOptions.filter.getYourPet.size[1];
+        }
+        if( sizeInput.includes("medium") ) {
+            api.getYourPet.search.params.SizeMin = 
+                api.getYourPet.search.params.SizeMin ?
+                api.getYourPet.search.params.SizeMin : 
+                dogOptions.filter.getYourPet.size[2];
+
+            api.getYourPet.search.params.SizeMax =  
+                dogOptions.filter.getYourPet.size[2];
+        }
+        if( sizeInput.includes("large") ) {
+            api.getYourPet.search.params.SizeMin = 
+                api.getYourPet.search.params.SizeMin ?
+                api.getYourPet.search.params.SizeMin : 
+                dogOptions.filter.getYourPet.size[3];
+
+            api.getYourPet.search.params.SizeMax =  
+                dogOptions.filter.getYourPet.size[3];
+        }
+        if( sizeInput.includes("xlarge") ) {
+            api.getYourPet.search.params.SizeMin = 
+                api.getYourPet.search.params.SizeMin ?
+                api.getYourPet.search.params.SizeMin : 
+                dogOptions.filter.getYourPet.size[4];
+
+            api.getYourPet.search.params.SizeMax =  
+                dogOptions.filter.getYourPet.size[4];
+        }
+    }
+}
+
+// Read user input on Zip-code filter
+function readZipCode() {
+    const zipInput = $(".js-zip-code").val();
     api.getYourPet.search.params.ZipCode = zipInput;
     api.petfinder.searchParams.location = zipInput;
 }
 
+function readFilters() {
+    cleanFilters();
+    readAge();
+    readCaretaker();
+    readGender();
+    readSize();
+    readZipCode();
+}
 
 function watchForm() {
-    $("form").submit(event => {
+    $(".searchForm").submit(event => {
         event.preventDefault();
-        updateZipCode();
+        readFilters();
         getPetList();
     });
 }
